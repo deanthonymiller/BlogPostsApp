7ffa3d18135afbbecca250987d61d248b95f78df
De'Anthony Miller <dmiller@nowmicro.com>
2019-08-14 18:48:00 -0500

    pushing what i got so far

---
+++ pages/about/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="about-page">
+        <h1>This is my absolutely fantastic "about me page"</h1>
+        <p>Lorem ispsu, dolor sit aet consectetur adipisicing elit. tomata</p>
+        <p>Lorem ispsu, dolor sit aet consectetur adipisicing elit. tomata</p>
+        <p>Lorem ispsu, dolor sit aet consectetur adipisicing elit. tomata</p>
+        <p>Lorem ispsu, dolor sit aet consectetur adipisicing elit. tomata</p>
+    </div>
+</template>
+<style scoped>
+about-page {
+    text-align: center;
+    padding: 30px;
+}
+</style>
---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="single-post-page"> 
+        <section class="post">
+            <h1>Title of the post</h1>
+            <div class="post-details">
+                <div>Last ipdate on XXX</div>
+                <div>Last ipdate on XXX</div>
+            </div>
+            <p>content of post</p>
+        </section>
+        <sections class="post-feedback">
+            <p>let me know what you think <a href="mailto:dmil1301@gmail.com">My Email</a>.</p>
+        </sections>
+    </div>
+</template>
